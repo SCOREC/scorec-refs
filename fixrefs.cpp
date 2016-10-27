d0d162dc1620c968c5e3b4c892559cafe766e070
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-10-27 12:51:34 -0400

    stop warning about article number

--- fixrefs.cpp
+++ fixrefs.cpp
@@ -664,7 +664,6 @@ static void warn_missing_fields(Entries& entries) {
       warn_missing_field(entries, entry, "year");
       warn_missing_field(entries, entry, "month");
       warn_missing_field(entries, entry, "volume");
-      warn_missing_field(entries, entry, "number");
       warn_missing_field(entries, entry, "pages");
     }
   }
